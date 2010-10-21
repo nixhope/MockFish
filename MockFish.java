a12088fcf53538ed1cf151419a1b75cdcc48f492
Jin Koo <darksentinel@gmail.com>
2010-10-22 08:31:39 +1300

    Tidied files, no major change.

--- MockFish.java
+++ MockFish.java
@@ -5,6 +5,6 @@
 import java.awt.*;
 public class MockFish {
 	public static void main( String[] args ) {
-		MockFishJFrame fishPanel = new MockFishJFrame( "HiJynx", 250, 100, c.WIDTH+15, c.HEIGHT+35 );
+		MockFishJFrame fishPanel = new MockFishJFrame( "MockFish", 250, 100, c.WIDTH+15, c.HEIGHT+35 );
 	}
 }
